aaf996351a32554f8c25edb9abe08e9a7aebd683
abhi-7-7 <aarsh4344@gmail.com>
2026-02-20 22:05:17 +0530

    statement update

--- ticket-booking-backend/src/server.js
+++ ticket-booking-backend/src/server.js
@@ -4,6 +4,7 @@ import app from './app.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 4000;
 
